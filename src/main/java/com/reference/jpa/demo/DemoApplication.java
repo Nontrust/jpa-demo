b6e513610f5f3ae056168c61b9365ee5c417e041
Nontrust <deed1515@naver.com>
2024-01-17 11:20:53 +0900

    [Getting Started] with simple entity / corresponding data repository  - run with commandline runner  - references: https://docs.spring.io/spring-data/jpa/reference/jpa/getting-started.html

--- src/main/java/com/reference/jpa/demo/DemoApplication.java
+++ src/main/java/com/reference/jpa/demo/DemoApplication.java
@@ -1,11 +1,32 @@
 package com.reference.jpa.demo;
 
+import com.reference.jpa.demo.entity.Person;
+import com.reference.jpa.demo.entity.PersonRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.NoSuchElementException;
 
 @SpringBootApplication
+@Slf4j
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(PersonRepository repository){
+        return args -> {
+            Person person = new Person();
+            person.setName("Nontrust");
+
+            repository.save(person);
+            Person persistPerson = repository.findById(person.getId())
+                    .orElseThrow(NoSuchElementException::new);
+            log.error("persistPerson {}", persistPerson.getId());
+        };
     }
 }
